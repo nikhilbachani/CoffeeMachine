5693a5db4b325f32896c0a97c2828a131c57ccb8
nikhilbachani <nikhilbachani25@gmail.com>
2020-11-15 14:38:21 -0500

    Implemented code to calculate possible cups of coffee

--- src/machine/Main.java
+++ src/machine/Main.java
@@ -19,12 +19,32 @@ public class Main {
 //        System.out.println("Coffee is ready!");
 
         /* Stage 2: Machines have needs */
-        System.out.println("Write how many cups of coffee you will need:");
-        int cupsOfCoffee = scanner.nextInt();
+//        System.out.println("Write how many cups of coffee you will need:");
+//        int cupsOfCoffee = scanner.nextInt();
+//
+//        System.out.printf("For %d cups of coffee you will need:%n", cupsOfCoffee);
+//        System.out.printf("%d ml of water%n", cupsOfCoffee * WATER_PER_CUP_ML);
+//        System.out.printf("%d ml of milk%n", cupsOfCoffee * MILK_PER_CUP_ML);
+//        System.out.printf("%d g of coffee beans%n", cupsOfCoffee * BEANS_PER_CUP_G);
 
-        System.out.printf("For %d cups of coffee you will need:%n", cupsOfCoffee);
-        System.out.printf("%d ml of water%n", cupsOfCoffee * WATER_PER_CUP_ML);
-        System.out.printf("%d ml of milk%n", cupsOfCoffee * MILK_PER_CUP_ML);
-        System.out.printf("%d g of coffee beans%n", cupsOfCoffee * BEANS_PER_CUP_G);
+        /* Stage 3: Enough coffee for everyone */
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int waterInMachine = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milkInMachine = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int beansInMachine = scanner.nextInt();
+
+        System.out.println("Write how many cups of coffee you will need:");
+        int cupsNeeded = scanner.nextInt();
+        int cupsPossible = Math.min(waterInMachine / WATER_PER_CUP_ML, Math.min(milkInMachine / MILK_PER_CUP_ML, beansInMachine / BEANS_PER_CUP_G));
+
+        if (cupsNeeded == cupsPossible) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (cupsNeeded < cupsPossible) {
+            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)%n", cupsPossible - cupsNeeded);
+        } else {
+            System.out.printf("No, I can make only %d cup(s) of coffee%n", cupsPossible);
+        }
     }
 }
